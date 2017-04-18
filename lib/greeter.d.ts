fa3f92a5b7fea412c427ccb27929b18ea05d33d5
Maximilian Alexander <mbalex99@gmail.com>
2017-04-18 09:11:28 -0700

    removing boilerplate cleanup

--- lib/greeter.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Greeter {
-    private greeting;
-    constructor(message: string);
-    greet(): string;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-export * from "./greeter";
+import * as program from 'commander'
 
-console.log('oh');
+var packageJSON = require('./package.json')
+var version = packageJSON.version as string
+var host: string = "localhost:9080"
+
+program
+    .version(version)
+    .command('use', 'sets and caches a realm object server endpoint. If the port is not included then port 9080 is used')
+    .action()
+    .option('-h, --host <hostnamewithport>', 'target a realm object server endpoint for this command', 'localhost:9080')
+    .option('-s, --service <servicename>', 'specifies a service name', /^(auth|func|realrest|all)$/i, 'all')
+    .parse(process.argv)
+
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ custom-help --help');
+  console.log('    $ custom-help -h');
+  console.log('');
+});
+
+program.parse(process.argv)
