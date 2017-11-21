4e78ab23775fbf23360326becfa1c40b84183a14
Maximilian Alexander <mbalex99@gmail.com>
2017-11-21 13:07:33 -0800

    fixing vscode debugging

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -16,12 +16,12 @@ describe('Greeter Tests', function () {
         expect(greeting).to.eq('hi')
     })
 
-    it('should say a number 3', function(){
+    it('should say a number 3', function () {
         const num = greeter.sayNumber()
         expect(num).to.eq(3)
     })
 
-    it('should say something with a timeout', function(done){
+    it('should say something with a timeout', function (done) {
         this.timeout(5000)
         greeter.saySomethingAsync((val) => {
             expect(val).to.eq('something')
@@ -29,13 +29,13 @@ describe('Greeter Tests', function () {
         })
     })
 
-    it('should say something with a timeout but with a promise now', async function(){
+    it('should say something with a timeout but with a promise now', async function () {
         this.timeout(5000)
         const message = await greeter.saySomethingPromised()
         expect(message).to.eq('I am wrapped in promise!')
     })
 
-    it('should say an exception message', function() {
+    it('should say an exception message', function () {
         expect(() => {
             greeter.forceACrash()
         }).to.throw('Some kind of custom crash message')
